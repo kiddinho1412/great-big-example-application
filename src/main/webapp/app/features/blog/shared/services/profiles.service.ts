e809c3536262a52ff9cd5c767fe1aaed328906ae
dancancro <dan.cancro@gmail.com>
2017-05-31 20:24:20 -0400

    wip

--- src/main/webapp/app/features/blog/shared/services/profiles.service.ts
+++ src/main/webapp/app/features/blog/shared/services/profiles.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { Store } from '@ngrx/store';
 
-import { ApiService } from './api.service';
+import * as fromRoot from '../../../../core/store';
+// import { ApiService } from './api.service';
 import { Profile } from '../../../../core/store/profile/profile.model';
+import { RESTService } from '../../../../core/services/rest.service';
 
 @Injectable()
 export class ProfilesService {
     constructor(
-        private apiService: ApiService
+        private store: Store<fromRoot.RootState>,
+        // private apiService: ApiService
     ) { }
 
     get(username: string): Observable<Profile> {
+
         return this.apiService.get('/profiles/' + username)
             .map((data: { profile: Profile }) => data.profile);
     }
